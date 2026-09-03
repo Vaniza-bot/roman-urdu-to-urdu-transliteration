29d2b2890a0e98b730c3039f0564347c3679e0e1
Vaniza Shahid <vaneezashahid372@gmail.com>
2026-09-03 13:16:05 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -199,8 +199,8 @@ def me():
     })
 
 # ---------------- INIT DB ----------------
-with app.app_context():
-    db.create_all()
+if __name__ == "__main__":
+    app.run()
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
